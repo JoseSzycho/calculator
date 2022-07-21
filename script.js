42f46e533fd5a69c9b08eba9cbda2ae8deb7b1bd
Jose Szychowski <joseszychowski@gmail.com>
2022-07-21 19:32:38 -0300

    Add event listener to all buttons

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let valuesToCalculate = { //values used for the calculus
+    operand: null,
+    firstValue: null,
+    secondValue: null,
+    result: null
+};
 
-const selector = document.querySelector(".operator");
-console.log(selector.attr('value'));
+function storeButtonValue(){
+    console.log(this.getAttribute("value"));
+    console.log(valuesToCalculate);
+    
+}
+
+const element = document.querySelectorAll("button"); //gets all button elements
+element.forEach(el => el.addEventListener("click", storeButtonValue)); //adds event listener to all elements
+//element.addEventListener("click", storeButtonValue);
