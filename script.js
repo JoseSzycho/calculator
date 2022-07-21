b5cd812b4b785a8ee6632c23f30d95cec8aacf2a
Jose Szychowski <joseszychowski@gmail.com>
2022-07-21 19:56:18 -0300

    Add function to store numbers in correct order

--- script.js
+++ script.js
@@ -6,9 +6,20 @@ let valuesToCalculate = { //values used for the calculus
 };
 
 function storeButtonValue(){
-    console.log(this.getAttribute("value"));
+    if(this.getAttribute("class") == "operator"){ 
+        if(valuesToCalculate.firstValue && !valuesToCalculate.secondValue){ //once you set firs number and second you cant change operator
+            valuesToCalculate.operand = this.getAttribute("value"); 
+        };
+    };
+    if(this.getAttribute("class") == "number"){  //if element is an number
+        if(!valuesToCalculate.secondValue){ //sets the first number 
+            valuesToCalculate.firstValue = parseInt(this.getAttribute("value"));
+        }
+        if(valuesToCalculate.firstValue && valuesToCalculate.operand){
+            valuesToCalculate.secondValue = parseInt(this.getAttribute("value")); //sets second number
+        }
+    }
     console.log(valuesToCalculate);
-    
 }
 
 const element = document.querySelectorAll("button"); //gets all button elements
