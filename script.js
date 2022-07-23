18f1623c216738d764bdc756e429a5a0fdae0e2a
Jose Szychowski <joseszychowski@gmail.com>
2022-07-23 15:25:36 -0300

    Add function for adding numbers, operator and display

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 let toCalculate = {
     firstNumber: null,
     operator: null,
-    secondNumber: null
+    secondNumber: null,
 };
 
-function addNumber(){
-    console.log(this)
-    if(!toCalculate.operator && !toCalculate.secondNumber){
-        toCalculate.firstNumber += this.getAttribute("value");
-    }
-
-    console.log(toCalculate.firstNumber);
+function secondaryDisplay(){
+    
 }
 
-const elements = document.querySelectorAll("button[class]:not(.equal, .operator)"); //gets all button elements
-elements.forEach(addEventListener("click", addNumber));
+function principalDisplay(value){
+    const element = document.querySelector("#principal-display");
+    element.textContent = value;
+    secondaryDisplay();
+}
+
+function addNumber(){
+    //adding first number
+    if(!toCalculate.operator && !toCalculate.secondNumber){
+        toCalculate.firstNumber += this.getAttribute("value");
+        toCalculate.firstNumber = toCalculate.firstNumber.replace('null','')
+        principalDisplay(toCalculate.firstNumber);
+    };
+    //adding second number
+    if(toCalculate.firstNumber && toCalculate.operator){
+        toCalculate.secondNumber += this.getAttribute("value");
+        toCalculate.secondNumber = toCalculate.secondNumber.replace('null','')
+        principalDisplay(toCalculate.secondNumber);
+    }
+    //console.log(toCalculate.firstNumber, toCalculate.operator, toCalculate.secondNumber)
+    
+}
+
+function selectOperator(){
+    if(toCalculate.firstNumber && !toCalculate.secondNumber){
+        toCalculate.operator = this.getAttribute("value");
+    }
+    principalDisplay(toCalculate.operator);
+}
+
+//adds event listener to numbers
+const numberElements = document.querySelectorAll("button[class]:not(.equal, .operator)"); //gets all button elements
+numberElements.forEach(el => el.addEventListener("click", addNumber));
+
+//ads event listener to operators
+const operatorElements = document.querySelectorAll("button[class]:not(.equal, .number)")
+operatorElements.forEach(el => el.addEventListener("click", selectOperator));
